5b4ea166a8d55024484181fe92977f6dcd0de19f
Griffin Maloney <241651177+gmaloney465@users.noreply.github.com>
2025-11-23 17:26:51 -0500

    Latest code from FLL Rumble
    Updated code from the FLL Rumble on 11/15/25.

--- mission_four.py
+++ mission_four.py
@@ -6,17 +6,18 @@ from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub()
 
-class MissionOne:
+#turn the bot around 
+class MissionFour:
     def Run(self, robot:DriveBase, left_attachment_motor, right_attachment_motor):
-        print("Mission One")
-        robot.straight(500)
-        robot.straight(-500)
-        wait(3000)
-        robot.straight(600)
-        # You need a wait before making the motors moving or the code runs and then before ti stars it stops 
-        left_attachment_motor.run_time(-400,800    )
-        wait(500)
-        robot.straight(-600)
-       
-
+        print("Mission Four ")
+        robot.straight(-730)
+        robot.turn(-120)
+        robot.turn(150)
+        robot.turn(-20)
+        robot.straight(255)
+        robot.turn(40)
+        robot.straight(100)
+        robot.straight(-50)
+        robot.turn(-90)
+        robot.straight(400)
         
--- mission_main.py
+++ mission_main.py
@@ -5,41 +5,67 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 from pybricks.robotics import DriveBase
-from pybricks.tools import hub_menu
 from mission_one import MissionOne
 from mission_two import MissionTwo
-
+from mission_three import MissionThree
+from mission_four import MissionFour
+from mission_five import MissionFive
 # Explicitly setup everything
 class MyMissions():
     @Mission(1)
-    def go_forward(robot, left_attachment_motor, right_attachment_motor):
+    def go_mission_one(robot, left_attachment_motor, right_attachment_motor):
         m = MissionOne()
         m.Run(robot, left_attachment_motor, right_attachment_motor)
 
     @Mission(2)
-    def go_reverse(robot, left_attachment_motor, right_attachment_motor):
+    def go_mission_two(robot, left_attachment_motor, right_attachment_motor):
         m = MissionTwo()
         m.Run(robot, left_attachment_motor, right_attachment_motor)
 
-hub = PrimeHub()
+    @Mission(3)
+    def go_mission_three(robot, left_attachment_motor, right_attachment_motor):
+        m = MissionThree()
+        m.Run(robot, left_attachment_motor, right_attachment_motor)
+
+
+    @Mission(4)
+    def go_mission_four(robot, left_attachment_motor, right_attachment_motor):
+        m = MissionFour()
+        m.Run(robot, left_attachment_motor, right_attachment_motor)
+
+
+    @Mission(5)
+    def go_mission_five(robot, left_attachment_motor, right_attachment_motor):
+        m = MissionFive()
+        m.Run(robot, left_attachment_motor, right_attachment_motor)
+
+
+hub = PrimeHub() 
+print(hub.battery.voltage())
 hub.display.orientation(Side.TOP)
 left_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.F, Direction.CLOCKWISE)
 robot = DriveBase(left_motor, right_motor,
-                  wheel_diameter=62.4, axle_track=20)
+                  wheel_diameter=62.4, axle_track=88)
 robot.use_gyro(True)
 
-left_attachment_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
-right_attachment_motor = Motor(Port.D, Direction.CLOCKWISE)
+left_attachment_motor = Motor(Port.B, Direction.CLOCKWISE,  [12, 20])
+right_attachment_motor = Motor(Port.D, Direction.CLOCKWISE, [12, 20])
 
 missions = MissionControl(MyMissions).use_robot(robot).use_left_attachment_motor(left_attachment_motor).use_right_attachment_motor(right_attachment_motor)
 
+
+missions.run()
+#.use_attachment_motors(left_attachment_motor, right_attachment_motor)
+#distance sensor is blue 
 # Unconditionally run all missions
 #for mission in missions:
-#    mission()
+    #mission()
 
 # Use selector to run all missions
-missions.run()
+
 
 # Don't put code here, MissionControl->run_all never returns
+
+
 
